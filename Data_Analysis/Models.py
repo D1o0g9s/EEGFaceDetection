c8dd1e292262f60b426387c107636bb00bce3f4b
gvalliap <ganeshv757@gmail.com>
2020-03-19 14:10:52 -0700

    updated models and neural net

--- Data_Analysis/Models.py
+++ Data_Analysis/Models.py
@@ -4,32 +4,32 @@ import torch.nn.functional as F
 from torch import optim
 
 class LSTM_Model(nn.Module):
-    def __init__(self, encode_dim, hidden_dim, batch_size, chunk_length, computing_device):
+    def __init__(self, input_dim, hidden_dim, num_classes, batch_size, computing_device):
         super(LSTM_Model, self).__init__()
-        self.encode_dim = encode_dim
+        self.input_dim = input_dim
         self.hidden_dim = hidden_dim
+        self.num_classes = num_classes
         self.batch_size = batch_size
-        self.chunk_length = chunk_length
-        self.computing_device = computing_device
+        self.device = computing_device
 
-        self.lstm = nn.LSTM(encode_dim, hidden_dim, batch_first=True, num_layers=1)
-        self.linear = nn.Linear(hidden_dim, encode_dim)
+        self.lstm = nn.LSTM(input_dim, hidden_dim, batch_first=True, num_layers=1)
+        self.linear = nn.Linear(hidden_dim, num_classes)
         self.hidden = self.init_hidden()
 
         self.forward_outputs = list()
-        self.hook_layers()
+#         self.hook_layers()
 
     def init_hidden(self):
-        return (torch.zeros(1, self.batch_size, self.hidden_dim).to(self.computing_device),
-                torch.zeros(1, self.batch_size, self.hidden_dim).to(self.computing_device))
+        self.hidden = (torch.zeros(1, self.batch_size, self.hidden_dim).to(self.device),
+                       torch.zeros(1, self.batch_size, self.hidden_dim).to(self.device))
 
-    def forward(self, chunk):
-        self.hidden = (self.hidden[0].detach(), self.hidden[1].detach())
-        chunk = chunk.reshape(1, *chunk.shape)
-        lstm_out, self.hidden = self.lstm(chunk, self.hidden)
-        output = self.linear(lstm_out)
+    def forward(self, x):
+        self.hidden = (self.hidden[0], self.hidden[1])
+#         chunk = chunk.reshape(1, *chunk.shape)
+        lstm_out, self.hidden = self.lstm(x, self.hidden)
+        output = self.linear(lstm_out[:, -1, :])
 
-        return output, self.hidden
+        return output
 
     def hook_layers(self):
         def forward_hook_function(module, tensor_in, tensor_out):
